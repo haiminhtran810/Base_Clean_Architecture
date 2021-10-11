9ebf5ff6b2dc4798fcea24b39cea9c760c6970c7
haitm <a39402300e498a99348615673b82aa63b5aa39a9>
2021-10-11 15:36:20 +0700

    [Task] Add UseCase

--- app/src/main/java/com/htm/base_clean_architecture/ui/splash/SplashViewModel.kt
+++ app/src/main/java/com/htm/base_clean_architecture/ui/splash/SplashViewModel.kt
@@ -5,14 +5,14 @@ import com.htm.base_clean_architecture.base.BaseViewModel
 import com.htm.domain.usecase.movie.GetMoviesUseCase
 import kotlinx.coroutines.launch
 
-class SplashViewModel() : BaseViewModel() {
+class SplashViewModel(private val moviesUseCase: GetMoviesUseCase) : BaseViewModel() {
     init {
         getTempMovies()
     }
 
     private fun getTempMovies() {
         viewModelScope.launch {
-            //moviesUseCase.executeWork(GetMoviesUseCase.Params(page = 0))
+            moviesUseCase.executeWork(GetMoviesUseCase.Params(page = 0))
         }
     }
 }
